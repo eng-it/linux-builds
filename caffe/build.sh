2df3c55d657b83009dedf77d1a41e2f3acfc2243
Jesse Connell <jesse08@bu.edu>
2016-07-05 17:13:06 -0400

    Fix #1: build latest unstable version for pycaffe

--- caffe/build.sh
+++ caffe/build.sh
@@ -2,6 +2,14 @@
 
 # Built for request in INC11959621
 #
+# In this latest attempt we're building straight from the master branch on
+# github due to problems with Boost.Python in release candidate 3.
+#
+# Building on budge02 succeeds.  Building on budge01 does not, possibly because
+# of the different compute capability that gets detected for nvcc for the GPU
+# cards in one system versus the other.  (budge01: M1060, budge02:
+# M2050/M2090.)  I haven't tested that idea, though.
+#
 # https://github.com/BVLC/caffe/releases
 # http://caffe.berkeleyvision.org/install_yum.html
 # http://caffe.berkeleyvision.org/installation.html#compilation
@@ -26,16 +34,16 @@ source $(dirname $0)/../common.sh
 
 module load boost
 module load ffmpeg
-# CUDA can't handle newer GCC versions
+# Our CUDA can't handle newer GCC versions
 module unload gcc
-module load caffe
+module load caffe/20160705
 source /ad/eng/opt/64/cuda/cuda-6.0/add_cuda-6.0.sh
 
-VER="rc3"
+VER="20160705"
 PREFIX="$ENGOPT/64/caffe-$VER"
 
 GFLAGS_VER="1.7"
-GLOG_VER="0.3.3"
+GLOG_VER="0.3.4"
 
 #OPENCV="/ad/eng/support/software/linux/opt/64/opencv/$VER"
 #MATLAB="/ad/eng/opt/matlab-8.5/"
@@ -57,6 +65,7 @@ function caffe()
 	make install 2>&1 | tee $WD/make_${name}_install.log
 }
 
+# switched to using git + CMAKE method instead.
 function caffe_cmake()
 {
 	name="caffe"
@@ -84,25 +93,64 @@ function caffe_cmake()
 	make runtest 2>&1 | tee $WD/make_${name}_runtest.log
 }
 
+# Third time's the charm!
+function caffe_cmake_git()
+{
+	name="caffe"
+	URL="$1"
+
+	git clone --depth=1 "$URL"
+	DIR=caffe
+
+	cd $DIR
+	mkdir -p build
+	cd build
+
+	# If we wanted matlab support, turn on these for cmake below:
+	#-D BUILD_matlab=ON \
+	#-D Matlab_mex=/ad/eng/bin/mex83 \
+	#-D Matlab_mexext=/ad/eng/bin/mexext83 \
+	cmake -D BLAS=open \
+	      -D CMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
+	      -D PYTHON_INCLUDE_DIR=$ANACONDA/include/python2.7 \
+	      -D PYTHON_LIBRARY=$ANACONDA/lib/libpython2.7.so \
+	      .. 2>&1 | tee $WD/cmake_${name}.log
+	make 2>&1 | tee $WD/make_${name}.log
+	make install 2>&1 | tee $WD/make_${name}_install.log
+	make runtest 2>&1 | tee $WD/make_${name}_runtest.log
+}
+
+# Install extra files for data, examples, etc.
+#
+# It looks like they assume you'll work directly from the build
+# directory and have read/write access to everything, but copying
+# example files to a separate directory seems to work OK too.
+#
+# Manual tasks after this:
+# - run data/mnist/get_mnist.sh
+# - modify  ./examples/mnist/create_mnist.sh to use correct binary
+# - run create_mnist.sh
+function caffe_files()
+{
+	DIR=$1
+	mkdir -p $PREFIX/caffe-files
+	for path in data examples models scripts
+	do
+		cp -ur $DIR/$path $PREFIX/caffe-files
+	done
+}
+
 function main()
 {
 	# Before caffe_cmake I ended up having to remove the local packages for
 	# gflags and gflags-devel, so that it would use our compiled version
 	# instead.  I'm not sure why that was necessary but re-installing those
 	# packages afterward it still seemed OK.
+
 	configure_make_install gflags "https://github.com/gflags/gflags/archive/v${GFLAGS_VER}.tar.gz"
 	configure_make_install glog "https://github.com/google/glog/archive/v${GLOG_VER}.tar.gz"
-	caffe_cmake "https://github.com/BVLC/caffe/archive/${VER}.tar.gz"
-	# Post-install I manually copied these directories into
-	# $PREFIX/caffe-files:
-	#  - data
-	#  - examples
-	#  - models
-	#  - scripts
-	# It looks like they assume you'll work directly from the build
-	# directory and have read/write access to everything, but copying
-	# example files to a separate directory seems to work OK too.
-
+	caffe_cmake_git "https://github.com/BVLC/caffe.git"
+	caffe_files caffe
 }
 
 if [[ ! $0 == "-bash" ]]
